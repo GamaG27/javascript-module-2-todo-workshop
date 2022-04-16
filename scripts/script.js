766ffface8e29d8ecfb7036b7d5fda2b1bd0d6e3
Damian <tricasaurio@gmail.com>
2022-04-16 18:53:39 +0200

    todo

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,26 @@
-alert("hello!");
+// alert("hello!");
 
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
-})
+// document.querySelector('#new-todo').addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const text = e.target.elements.text.value.trim();
+//     alert(text);
+// })
+
+const buttons = document.querySelector("#create-button");
+console.log(buttons);
+
+const check = document.querySelectorAll(".checkbox");
+console.log(check);
+
+const createButton = document.querySelector("#create-button");
+
+//bur
+//createButton.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     console.log("Someone just clicked me once!");
+// });
+
+// createButton.addEventListener("dblclick", (event) => {
+//     event.preventDefault();
+//     console.log("Someone just clicked me twice!");
+// });
